6ce9186741182d2bc7e298009931395b15470260
Josephus Paye II <j.paye96@gmail.com>
2020-03-18 21:05:40 +1100

    Show sidebar panel only on embeddables (not posts and pages)

--- src/index.js
+++ src/index.js
@@ -1,71 +1,78 @@
 import { CheckboxControl } from '@wordpress/components';
 import { compose } from '@wordpress/compose';
-import { withSelect, withDispatch } from '@wordpress/data';
+import { select, withSelect, withDispatch } from '@wordpress/data';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { registerPlugin } from '@wordpress/plugins';
+import domReady from '@wordpress/dom-ready';
 
-const mapSelectToProps = (select, props) => {
-    return {
-        metaFieldValue: select('core/editor').getEditedPostAttribute('meta')[
-            props.metaFieldName
-        ],
-    };
-};
+domReady(() => {
+    const postType = select('core/editor').getCurrentPostType();
 
-const mapDispatchToProps = (dispatch, props) => {
-    return {
-        setMetaFieldValue: function(value) {
-            dispatch('core/editor').editPost({
-                meta: { [props.metaFieldName]: value },
-            });
-        },
-    };
-};
+    if (postType === 'embeddable') {
+        const mapSelectToProps = (select, props) => {
+            return {
+                metaFieldValue: select('core/editor').getEditedPostAttribute(
+                    'meta'
+                )[props.metaFieldName],
+            };
+        };
 
-const CheckboxField = props => {
-    return (
-        <CheckboxControl
-            label={props.label}
-            help={props.help}
-            checked={props.metaFieldValue}
-            onChange={value => {
-                props.setMetaFieldValue(value);
-            }}
-        />
-    );
-};
+        const mapDispatchToProps = (dispatch, props) => {
+            return {
+                setMetaFieldValue: function(value) {
+                    dispatch('core/editor').editPost({
+                        meta: { [props.metaFieldName]: value },
+                    });
+                },
+            };
+        };
 
-const DisableWpHeadField = compose(
-    withDispatch(mapDispatchToProps),
-    withSelect(mapSelectToProps)
-)(CheckboxField);
+        const CheckboxField = props => {
+            return (
+                <CheckboxControl
+                    label={props.label}
+                    help={props.help}
+                    checked={props.metaFieldValue}
+                    onChange={value => {
+                        props.setMetaFieldValue(value);
+                    }}
+                />
+            );
+        };
 
-const DisableWpFooterField = compose(
-    withDispatch(mapDispatchToProps),
-    withSelect(mapSelectToProps)
-)(CheckboxField);
+        const DisableWpHeadField = compose(
+            withDispatch(mapDispatchToProps),
+            withSelect(mapSelectToProps)
+        )(CheckboxField);
 
-const SidebarPanel = () => {
-    return (
-        <PluginDocumentSettingPanel
-            name="wp-embeddable-sidebar"
-            title="Embeddable Options"
-            icon={() => ''}
-        >
-            <DisableWpHeadField
-                label="Disable wp_head()"
-                help="Disable scripts and styles from the page header"
-                metaFieldName="wp_embeddable_disable_wp_head"
-            />
-            <DisableWpFooterField
-                label="Disable wp_footer()"
-                help="Disable scripts and styles from the page footer"
-                metaFieldName="wp_embeddable_disable_wp_footer"
-            />
-        </PluginDocumentSettingPanel>
-    );
-};
+        const DisableWpFooterField = compose(
+            withDispatch(mapDispatchToProps),
+            withSelect(mapSelectToProps)
+        )(CheckboxField);
 
-registerPlugin('wp-embeddable-sidebar', {
-    render: SidebarPanel,
+        const SidebarPanel = () => {
+            return (
+                <PluginDocumentSettingPanel
+                    name="wp-embeddable-sidebar"
+                    title="Embeddable Options"
+                    icon={() => ''}
+                >
+                    <DisableWpHeadField
+                        label="Disable wp_head()"
+                        help="Disable scripts and styles from the page header"
+                        metaFieldName="wp_embeddable_disable_wp_head"
+                    />
+                    <DisableWpFooterField
+                        label="Disable wp_footer()"
+                        help="Disable scripts and styles from the page footer"
+                        metaFieldName="wp_embeddable_disable_wp_footer"
+                    />
+                </PluginDocumentSettingPanel>
+            );
+        };
+
+        registerPlugin('wp-embeddable-sidebar', {
+            render: SidebarPanel,
+        });
+    }
 });
