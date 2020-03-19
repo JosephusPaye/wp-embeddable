5d4351ea48986dd96fc2e7f5e71fb0e6bcf68985
Josephus Paye II <j.paye96@gmail.com>
2020-03-19 23:15:40 +1100

    Add embeddable usage metabox

--- plugin/classic-metabox.php
+++ plugin/classic-metabox.php
@@ -7,6 +7,12 @@ $wpEmbeddableMetaBox = [
     'location' => 'side',
 ];
 
+$wpEmbeddableShortCodeMetaBox = [
+    'key' => 'wp_embeddable_shortcode_metabox',
+    'label' => 'Embeddable Usage',
+    'location' => 'side',
+];
+
 $wpEmbeddableOptionsMetaboxHtml = function ($post) use ($wpEmbeddableMetaFields, $wpEmbeddableMetaBox) {
     wp_nonce_field($wpEmbeddableMetaBox['key'], $wpEmbeddableMetaBox['nonce']);
 
@@ -20,17 +26,42 @@ $wpEmbeddableOptionsMetaboxHtml = function ($post) use ($wpEmbeddableMetaFields,
                 <input value="1" type="checkbox" name="<?php echo $fieldName ?>" <?php echo boolval($fieldValue) ? 'checked="checked"' : '' ?>> <?php echo $fieldOptions['help'] ?>
             </label>
         </div>
-<?php
+    <?php
     }
 };
 
-add_action('add_meta_boxes', function () use ($wpEmbeddablePostTypeKey, $wpEmbeddableMetaBox, $wpEmbeddableOptionsMetaboxHtml) {
+$wpEmbeddableShortCodeMetaboxHtml = function ($post) {
+    $shortcode = '[embeddable ' .  $post->ID . ' autosize]';
+    ?>
+    <p class="post-attributes-label-wrapper"><label class="post-attributes-label" for="wp_embeddable_shortcode">Shortcode</label></p>
+    <input class="widefat" id="wp_embeddable_shortcode" readonly type="text" value="<?php echo esc_attr($shortcode); ?>">
+
+    <p class="post-attributes-label-wrapper"><label class="post-attributes-label" for="wp_embeddable_embed_code">Embed code</label></p>
+    <textarea class="widefat" rows="4" id="wp_embeddable_embed_code" readonly><?php echo esc_html(do_shortcode($shortcode)); ?></textarea>
+<?php
+};
+
+add_action('add_meta_boxes', function () use (
+    $wpEmbeddablePostTypeKey,
+    $wpEmbeddableMetaBox,
+    $wpEmbeddableOptionsMetaboxHtml,
+    $wpEmbeddableShortCodeMetaBox,
+    $wpEmbeddableShortCodeMetaboxHtml
+) {
     add_meta_box(
         $wpEmbeddableMetaBox['key'],
         $wpEmbeddableMetaBox['label'],
         $wpEmbeddableOptionsMetaboxHtml,
         $wpEmbeddablePostTypeKey,
         $wpEmbeddableMetaBox['location']
+    );
+
+    add_meta_box(
+        $wpEmbeddableShortCodeMetaBox['key'],
+        $wpEmbeddableShortCodeMetaBox['label'],
+        $wpEmbeddableShortCodeMetaboxHtml,
+        $wpEmbeddablePostTypeKey,
+        $wpEmbeddableShortCodeMetaBox['location']
     );
 });
 
